e599f1f5fa6b9b4dc16ad1981bab36e4d51abbfb
francoandre.dev <francoandre.dev@gmail.com>
2024-10-20 13:03:46 -0500

    build: set up inicial de api sazón y sabor

---
+++ src/test/java/com/sazonysabor/api/SazonysaborApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sazonysabor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SazonysaborApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
